6a4ea3f6b5388623a98fdf768fbe35888312a346
Jan Mechtel <jmechtel@gmail.com>
2014-11-27 22:38:08 +0100

    moved addCheck Function to the main Controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,16 @@
  */
 angular.module('checkerApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.addCheck = function(type) {
+      var currentUser = Parse.User.current();
+      if (currentUser) {
+        var Check = Parse.Object.extend("Check");
+        var check = new Check();
+        check.set("type", type);
+        check.setACL(new Parse.ACL(currentUser));
+        check.save();
+      } else {
+        alert('Login first!')
+      }
+    };
   });
