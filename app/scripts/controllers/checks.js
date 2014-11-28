9e5890e86139c68a8a5579c674ca7b8493ff2a65
Jan Mechtel <jmechtel@gmail.com>
2014-11-28 22:40:28 +0100

    Add food as check type

--- app/scripts/controllers/checks.js
+++ app/scripts/controllers/checks.js
@@ -9,20 +9,28 @@
  */
 angular.module('checkerApp')
   .controller('ChecksCtrl', function ($scope) {
-    var Check = Parse.Object.extend('Check');
-    var collection = Parse.Collection.extend({
-      model: Check
-    });
-    $scope.checks = new collection();
-    $scope.checks.query = new Parse.Query(Check);
-    $scope.checks.fetch();
+    $scope.init = function(type)
+    {
+      //This function is sort of private constructor for controller
+      $scope.type = type;
+      var check = Parse.Object.extend('Check');
+      var collection = Parse.Collection.extend({
+        model: check
+      });
+      $scope.checks = new collection();
+      $scope.checks.query = new Parse.Query(check);
+      $scope.checks.query.equalTo('type', $scope.type);
+      $scope.checks.fetch();
+    };
 
-    $scope.addCheck = function(type) {
+
+
+    $scope.addCheck = function() {
       var currentUser = Parse.User.current();
       if (currentUser) {
         var Check = Parse.Object.extend('Check');
         var check = new Check();
-        check.set('type', type);
+        check.set('type', $scope.type);
         check.setACL(new Parse.ACL(currentUser));
         check.save();
         $scope.checks.fetch();
